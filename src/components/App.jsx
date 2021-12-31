cbf9d9387af76aaaa7cb7bbb6027ba71af54654e
mkjun14 <83442556+mkjun14@users.noreply.github.com>
2021-12-31 01:30:38 -0600

    Keeper App part 2 finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
+
+// const newNote = notes.map((note)=>)
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note
+        title="This is the note title" content="This is the content"
+      /> */}
+
+      {notes.map((noteItem) => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />)}
       <Footer />
     </div>
   );
